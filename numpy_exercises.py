5455793116e84d2b0cf93482ac8662138b851891
lopezelihezer <lopezelihezer@gmail.com>
2021-07-15 16:00:53 -0500

    Added more exercises

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -91,20 +91,33 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Use python's built in functionality/operators to determine the following:
 # Exercise 1 - Make a variable called sum_of_a to hold the sum of all the numbers in above list
+sum_of_a = numpy.sum(a)
+
 
 # Exercise 2 - Make a variable named min_of_a to hold the minimum of all the numbers in the above list
+min_of_a = numpy.min(a)
+
 
 # Exercise 3 - Make a variable named max_of_a to hold the max number of all the numbers in the above list
+max_of_a = numpy.max(a)
+
 
 # Exercise 4 - Make a variable named mean_of_a to hold the average of all the numbers in the above list
+mean_of_a = numpy.mean(a)
+
 
 # Exercise 5 - Make a variable named product_of_a to hold the product of multiplying all the numbers in the above list together
+product_of_a = numpy.prod(a)
 
 # Exercise 6 - Make a variable named squares_of_a. It should hold each number in a squared like [1, 4, 9, 16, 25...]
+b = numpy.array(a)
+squares_of_a = b**2
 
 # Exercise 7 - Make a variable named odds_in_a. It should hold only the odd numbers
+odds_in_a = b[b%2==1]
 
 # Exercise 8 - Make a variable named evens_in_a. It should hold only the evens.
+evens_in_a = b[b%2==0]
 
 ## What about life in two dimensions? A list of lists is matrix, a table, a spreadsheet, a chessboard...
 ## Setup 2: Consider what it would take to find the sum, min, max, average, sum, product, and list of squares for this list of two lists.
@@ -117,16 +130,26 @@ b = [
 sum_of_b = 0
 for row in b:
     sum_of_b += sum(row)
+####
+b_array = numpy.array(b)
+sum_of_b = b_array.sum()
 
 # Exercise 2 - refactor the following to use numpy. 
 min_of_b = min(b[0]) if min(b[0]) <= min(b[1]) else min(b[1])  
+###
+min_of_b = b.array.min()
 
 # Exercise 3 - refactor the following maximum calculation to find the answer with numpy.
 max_of_b = max(b[0]) if max(b[0]) >= max(b[1]) else max(b[1])
+###
+max_of_b = b_array.max()
 
 
 # Exercise 4 - refactor the following using numpy to find the mean of b
 mean_of_b = (sum(b[0]) + sum(b[1])) / (len(b[0]) + len(b[1]))
+###
+mean_of_b = b_array.mean()
+
 
 # Exercise 5 - refactor the following to use numpy for calculating the product of all numbers multiplied together.
 product_of_b = 1
@@ -134,11 +157,17 @@ for row in b:
     for number in row:
         product_of_b *= number
 
+###
+product_of_b = b_array.prod()
+
 # Exercise 6 - refactor the following to use numpy to find the list of squares 
 squares_of_b = []
 for row in b:
     for number in row:
         squares_of_b.append(number**2)
+
+###
+squares_of_b = b_array**2
 
 
 # Exercise 7 - refactor using numpy to determine the odds_in_b
@@ -148,6 +177,10 @@ for row in b:
         if(number % 2 != 0):
             odds_in_b.append(number)
 
+###
+odds_in_b = b_array[b_array%2==1]
+
+
 
 # Exercise 8 - refactor the following to use numpy to filter only the even numbers
 evens_in_b = []
@@ -156,13 +189,21 @@ for row in b:
         if(number % 2 == 0):
             evens_in_b.append(number)
 
+###
+evens_in_b = b_array[b_array%2==0]
+
+
 # Exercise 9 - print out the shape of the array b.
+print(b_array)
 
 # Exercise 10 - transpose the array b.
+numpy.transpose(b_array)
 
 # Exercise 11 - reshape the array b to be a single list of 6 numbers. (1 x 6)
+b_array.reshape(1, 6)
 
 # Exercise 12 - reshape the array b to be a list of 6 lists, each containing only 1 number (6 x 1)
+b_array.reshape(6, 1)
 
 ## Setup 3
 c = [
